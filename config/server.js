fe797e898e8f8858c103efeaa18873445ecffa99
victorfernandesraton <vfbraton@gmail.com>
2020-04-24 00:41:50 -0300

    cors fix

--- config/server.js
+++ config/server.js
@@ -1,5 +1,12 @@
 const restify = require('restify');
 const plugins = require('restify').plugins
+const corsMiddleware = require("restify-cors-middleware");
+
+const cors = corsMiddleware({
+  origins: ["*"],
+  allowHeaders: ["Authorization"],
+  exposeHeaders: ["Authorization"]
+});
 
 const app = restify.createServer({
   name: "thingYota-api"
@@ -10,5 +17,8 @@ app.use(plugins.acceptParser(app.acceptable));
 app.use(plugins.jsonp({ mapParams: true }));
 app.use(plugins.queryParser({ mapParams: true }));
 app.use(plugins.fullResponse());
+
+app.pre(cors.preflight);
+app.use(cors.actual);
 
 module.exports = app;
