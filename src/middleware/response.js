c096561d5e2d54d4cfd0001f9d698b2446a7aa00
victorfernandesraton <vfbraton@gmail.com>
2021-07-18 20:17:59 -0300

    fix: import error

--- src/middleware/response.js
+++ src/middleware/response.js
@@ -1,11 +1,13 @@
+const errors = require("restify-errors");
+
 const responseOk = (req, res, send) => {
   try {
     const { data } = req.locals;
-     res.send(200, {
+    res.send(200, {
       data: data,
     });
   } catch (error) {
-     res.send(new errors.InternalServerError(`${error}`));
+    res.send(new errors.InternalServerError(`${error}`));
   }
 };
 
